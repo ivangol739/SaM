6a813f9d490c0aba07bc7fa7eef039a096d3195c
ivangol739 <ivangol739@gmail.com>
2024-07-23 23:58:32 +0300

    add str method

--- StudentsAndMentors.py
+++ StudentsAndMentors.py
@@ -7,6 +7,14 @@ class Student:
 		self.courses_in_progress = []
 		self.grades = {}
 
+	def __str__(self):
+		res = f"{self.name}\n"
+		res += f"{self.surname}\n"
+		res += f"Средняя оценка за домашние задания: {self.average_grade():1f}\n"
+		res += f"Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n"
+		res += f"Завершенные курсы: {", ".join(self.finished_courses)}"
+		return res
+
 	def rate_hw(self, lecturer, course, grade):
 		if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
 			if course in lecturer.grades:
@@ -15,6 +23,9 @@ class Student:
 				lecturer.grades[course] = [grade]
 		else:
 			return 'Ошибка'
+
+	def average_grade(self):
+		return 0
 
 
 class Mentor:
@@ -29,10 +40,19 @@ class Lecturer(Mentor):
 		super().__init__(name, surname)
 		self.grades = {}
 
+	def __str__(self):
+		res = f"{self.name}\n"
+		res += f"{self.surname}\n"
+		res += f"Средняя оценка за лекции: {self.average_grade():1f}\n"
+		return res
 
 class Reviewer(Mentor):
 	def __init__(self, name, surname):
 		super().__init__(name, surname)
+
+	def __str__(self):
+		res = f"{self.name}\n{self.surname}"
+		return res
 
 	def rate_hw(self, student, course, grade):
 		if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -42,3 +62,8 @@ class Reviewer(Mentor):
 				student.grades[course] = [grade]
 		else:
 			return 'Ошибка'
+
+
+student1 = Student("Ivan", "Golovachev", "male")
+print(student1)
+reviewer1 = Reviewer("Олег", "Булыгин")
