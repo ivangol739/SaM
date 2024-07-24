e8f1aea2efc25051433d4f8086dc93614fc9d8e7
ivangol739 <ivangol739@gmail.com>
2024-07-24 23:16:15 +0300

    функция подсчета средней оценки

--- StudentsAndMentors.py
+++ StudentsAndMentors.py
@@ -10,9 +10,9 @@ class Student:
 	def __str__(self):
 		res = f"{self.name}\n"
 		res += f"{self.surname}\n"
-		res += f"Средняя оценка за домашние задания: {self.average_grade():1f}\n"
+		res += f"Средняя оценка за домашние задания: {self.average_grade(): .1f}\n"
 		res += f"Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n"
-		res += f"Завершенные курсы: {", ".join(self.finished_courses)}"
+		res += f"Завершенные курсы: {", ".join(self.finished_courses)}\n"
 		return res
 
 	def rate_hw(self, lecturer, course, grade):
@@ -25,7 +25,10 @@ class Student:
 			return 'Ошибка'
 
 	def average_grade(self):
-		return 0
+		total_grade = []
+		for grade in self.grades.values():
+			total_grade.extend(grade)
+		return sum(total_grade) / len(total_grade) if total_grade else 0
 
 
 class Mentor:
@@ -43,15 +46,21 @@ class Lecturer(Mentor):
 	def __str__(self):
 		res = f"{self.name}\n"
 		res += f"{self.surname}\n"
-		res += f"Средняя оценка за лекции: {self.average_grade():1f}\n"
+		res += f"Средняя оценка за лекции: {self.average_grade(): .1f}\n"
 		return res
+
+	def average_grade(self):
+		total_grade = []
+		for grade in self.grades.values():
+			total_grade.extend(grade)
+		return sum(total_grade) / len(total_grade)
 
 class Reviewer(Mentor):
 	def __init__(self, name, surname):
 		super().__init__(name, surname)
 
 	def __str__(self):
-		res = f"{self.name}\n{self.surname}"
+		res = f"{self.name}\n{self.surname}\n"
 		return res
 
 	def rate_hw(self, student, course, grade):
@@ -64,6 +73,49 @@ class Reviewer(Mentor):
 			return 'Ошибка'
 
 
-student1 = Student("Ivan", "Golovachev", "male")
+student1 = Student("Иван", "Головачев", "male")
+lecturer1 = Lecturer("Олег", "Булыгин")
+reviewer1 = Reviewer("Сергей", "Сергеев")
+student2 = Student("Валерия", "Головачева", "female")
+lecturer2 = Lecturer("Тимур", "Анвартдинов")
+reviewer2 = Reviewer("Николай", "Николаев")
+
+
+student1.finished_courses.append("Введение в программирование")
+student1.courses_in_progress.append("Python")
+student1.courses_in_progress.append("Git")
+student2.finished_courses.append("Основы Python")
+student2.courses_in_progress.append("Python")
+student2.courses_in_progress.append("Git")
+
+lecturer1.courses_attached.append("Python")
+lecturer2.courses_attached.append("Git")
+reviewer1.courses_attached.append("Python")
+reviewer2.courses_attached.append("Git")
+
+reviewer1.rate_hw(student1, "Python", 10)
+reviewer1.rate_hw(student1, "Python", 8)
+reviewer2.rate_hw(student1, "Git", 9)
+reviewer2.rate_hw(student1, "Git", 9)
+
+reviewer1.rate_hw(student2, "Python", 6)
+reviewer1.rate_hw(student2, "Python", 8)
+reviewer2.rate_hw(student2, "Git", 6)
+reviewer2.rate_hw(student2, "Git", 7)
+
+student1.rate_hw(lecturer1, "Python", 10)
+student1.rate_hw(lecturer1, "Git", 8)
+student1.rate_hw(lecturer2, "Python", 4)
+student1.rate_hw(lecturer2, "Git", 5)
+
+student2.rate_hw(lecturer1, "Python", 8)
+student2.rate_hw(lecturer1, "Git", 6)
+student2.rate_hw(lecturer2, "Python", 8)
+student2.rate_hw(lecturer2, "Git", 6)
+
 print(student1)
-reviewer1 = Reviewer("Олег", "Булыгин")
+print(student2)
+print(reviewer1)
+print(reviewer2)
+print(lecturer1)
+print(lecturer2)
